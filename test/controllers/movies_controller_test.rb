237eb3cb318989bba5e8776de609916c3100065d
cnpanic <cnpanic@naver.com>
2017-05-02 04:49:42 +0000

    0502

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
-class MoviesControllerTest < ActionController::TestCase
+class MoviesControllerTest < ActionController::IntegrationTest
+  test "should not save article without title" do
+    movie1 = Movie.new
+    assert_not movie1.save, "Saved the movie without a title"
+  end
   # test "the truth" do
   #   assert true
   # end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -4,4 +4,20 @@ class MovieTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+      fixtures :movies
+    def test_validity_of_title
+        
+        movie1 = Movie.new({title:"La la land",information:"Blah Blah"})
+        assert movie1.valid?
+        
+        movie2 = Movie.new({title:"Hey",information:""})
+        assert !movie2.valid?, "Title word too short."
+        
+        movie3 = Movie.new
+        assert !movie3.valid?, "Empty data item is created."
+        
+        movie4 = Movies(:big)
+        puts "Motive title is #{movie4.title}"
+        assert_equal movie4.title, "My Big Daddy" 
+    end
 end
